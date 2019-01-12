c9d14ac24cdf3e57278eb5a83d00e4a7a54676cf
= <=>
2019-01-12 18:35:05 +0100

    added documentation of the dataset

---
+++ R/doc_data.R
@@ -0,0 +1,20 @@
+#' Values of log10 to interpolate
+#'
+#' The data is generated with R, such that the first column are the values from
+#' 1 to 100 in steps of 0.01. The second colum ist the log10 function from R of
+#' the first column.
+#'
+#' @docType data
+#'
+#' @usage data(log10data)
+#'
+#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(log10data)
+#' k <- log10data$k
+#' logk <- log10data$logk
+#' \donttest{plot(k, logk)}
+"log10data"
