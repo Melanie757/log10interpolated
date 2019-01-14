367ecad27048bf53d54e6413c29d8a2e20e17831
= <=>
2019-01-14 17:43:34 +0100

    added right format

--- R/doc_data.R
+++ R/doc_data.R
@@ -1,14 +1,14 @@
-#' Values of log10 to interpolate
+#' Values of log10 to interpolate between
 #'
 #' The data is generated with R, such that the first column are the values from
-#' 1 to 100 in steps of 0.01. The second colum ist the log10 function from R of
+#' 1 to 100 in steps of 0.01. The second colum is the log10 function from R of
 #' the first column.
 #'
 #' @docType data
 #'
 #' @usage data(log10data)
 #'
-#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#' @format A dataframe with 9901 rows and 2 columns.
 #'
 #' @keywords datasets
 #'
